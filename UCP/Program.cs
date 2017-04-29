1865ac6727a79e8066846154d857b975385ffaa1
DESKTOP-0TPADVQ\Bryan <bryanvdistefano@gmail.com>
2017-04-29 14:24:17 -0700

    Fixed and added 2 methods (deleteMath and deleteDependingOnIndex)

--- UCP/Program.cs
+++ UCP/Program.cs
@@ -22,6 +22,7 @@ namespace UCP
                 List<String> InputList = InputVar.Split(' ').ToList();
                 while (InputList.Count != 1)
                 {
+                    startOfLoop:
                     noParens = true;
                     for (int parensCheck = 0; parensCheck < InputList.Count(); parensCheck++)
                     {
@@ -43,6 +44,18 @@ namespace UCP
                                 {
                                     if (InputList[y] == "(")
                                     {
+                                        if (z - y == 1)
+                                        {
+                                            InputList.Remove(InputList[y]);
+                                            InputList.Remove(InputList[y]);
+                                            goto startOfLoop;
+                                        }
+                                        if (z - y == 2)
+                                        {
+                                            InputList.Remove(InputList[y]);
+                                            InputList.Remove(InputList[y + 1]);
+                                            goto startOfLoop;
+                                        }
                                         parenStart = y;
                                         noMultOrDiv = true;
                                         for (int n = parenStart; n < parenEnd; n++)
@@ -67,29 +80,16 @@ namespace UCP
                                             {
                                                 if (InputList[p] == "x")
                                                 {
-                                                    InputList[p - 2] = int.Parse(InputList[p - 1]) * int.Parse(InputList[p + 1]) + "";
-                                                    sum = int.Parse(InputList[p - 2]);
-
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    y = y - 1;
-                                                    z = z - 2;
-                                                    break;
-                                                }
+                                                InputList[p - 1] = int.Parse(InputList[p - 1]) * int.Parse(InputList[p + 1]) + "";
+                                                deleteMath(p, InputList);
+                                                goto startOfLoop;
+                                            }
                                                 if (InputList[p] == "/")
                                                 {
-                                                    InputList[p - 2] = int.Parse(InputList[p - 1]) / int.Parse(InputList[p + 1]) + "";
-                                                    sum = int.Parse(InputList[p - 2]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    y = y - 1;
-                                                    z = z - 2;
-                                                    break;
-                                                }
+                                                    InputList[p - 1] = int.Parse(InputList[p - 1]) / int.Parse(InputList[p + 1]) + "";
+                                                deleteMath(p, InputList);
+                                                goto startOfLoop;
+                                            }
                                                                                               
                                             }
 
@@ -97,22 +97,16 @@ namespace UCP
                                             {
                                                 if (InputList[p] == "+")
                                                 {
-                                                    InputList[p - 2] = int.Parse(InputList[p - 1]) + int.Parse(InputList[p + 1]) + "";
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    break;
-                                                }
+                                                InputList[p - 1] = int.Parse(InputList[p - 1]) + int.Parse(InputList[p + 1]) + "";
+                                                deleteMath(p, InputList);
+                                                goto startOfLoop;
+                                            }
                                                 if (InputList[p] == "-")
                                                 {
-                                                    InputList[p - 2] = int.Parse(InputList[p - 1]) - int.Parse(InputList[p + 1]) + "";
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    InputList.Remove(InputList[p - 1]);
-                                                    break;
-                                                }
+                                                    InputList[p - 1] = int.Parse(InputList[p - 1]) - int.Parse(InputList[p + 1]) + "";
+                                                deleteMath(p, InputList);
+                                                goto startOfLoop;
+                                            }
                                             }
                                         }
                                     }
@@ -151,33 +145,15 @@ namespace UCP
                                 {
                                     InputList[b] = int.Parse(InputList[b - 1]) * int.Parse(InputList[b + 1]) + "";
                                     sum = int.Parse(InputList[b]);
-                                    if (b != 0)
-                                    {
-                                        InputList.Remove(InputList[b - 1]);
-                                        InputList.Remove(InputList[b]);
-                                    }
-                                    else
-                                    {
-                                        InputList.Remove(InputList[b + 1]);
-                                        InputList.Remove(InputList[b]);
-                                    }
-                                    break;
+                                    deleteDependingOnIndex(b, InputList);
+                                    goto startOfLoop;
                                 }
                                 else if (InputList[b] == "/")
                                 {
                                     InputList[b] = int.Parse(InputList[b - 1]) / int.Parse(InputList[b + 1]) + "";
                                     sum = int.Parse(InputList[b]);
-                                    if (b != 0)
-                                    {
-                                        InputList.Remove(InputList[b - 1]);
-                                        InputList.Remove(InputList[b]);
-                                    }
-                                    else
-                                    {
-                                        InputList.Remove(InputList[b + 1]);
-                                        InputList.Remove(InputList[b]);
-                                    }
-                                    break;
+                                    deleteDependingOnIndex(b, InputList);
+                                    goto startOfLoop;
                                 }
                             }
 
@@ -187,33 +163,15 @@ namespace UCP
                                 {
                                     InputList[b] = int.Parse(InputList[b - 1]) + int.Parse(InputList[b + 1]) + "";
                                     sum = int.Parse(InputList[b]);
-                                    if (b != 0)
-                                    {
-                                        InputList.Remove(InputList[b - 1]);
-                                        InputList.Remove(InputList[b]);
-                                    }
-                                    else
-                                    {
-                                        InputList.Remove(InputList[b + 1]);
-                                        InputList.Remove(InputList[b]);
-                                    }
-                                    break;
+                                    deleteDependingOnIndex(b, InputList);
+                                    goto startOfLoop;
                                 }
                                 else if (InputList[b] == "-")
                                 {
                                     InputList[b] = int.Parse(InputList[b - 1]) - int.Parse(InputList[b + 1]) + "";
                                     sum = int.Parse(InputList[b]);
-                                    if (b != 0)
-                                    {
-                                        InputList.Remove(InputList[b - 1]);
-                                        InputList.Remove(InputList[b]);
-                                    }
-                                    else
-                                    {
-                                        InputList.Remove(InputList[b + 1]);
-                                        InputList.Remove(InputList[b]);
-                                    }
-                                    break;
+                                    deleteDependingOnIndex(b, InputList);
+                                    goto startOfLoop;
                                 }
 
                             }
@@ -221,14 +179,6 @@ namespace UCP
                         }
 
                     }
-                    if(InputList.Count == 3 )
-                        if(InputList[0] == "(")
-                        {
-                            {
-                                InputList.Remove(InputList[0]);
-                                InputList.Remove(InputList[1]);
-                            }
-                        }
 
                     if (InputList.Count == 1)
                     {
@@ -238,6 +188,25 @@ namespace UCP
                   }
                 }
             }
+        public static void deleteMath(int p, List<string> InputList)
+        {
+            InputList.Remove(InputList[p]);
+            InputList.Remove(InputList[p]);
+
+        }
+        public static void deleteDependingOnIndex(int b, List<string> InputList)
+        {
+            if (b != 0)
+            {
+                InputList.Remove(InputList[b - 1]);
+                InputList.Remove(InputList[b]);
+            }
+            else
+            {
+                InputList.Remove(InputList[b + 1]);
+                InputList.Remove(InputList[b]);
+            }
+        }
         }
     } 
 
